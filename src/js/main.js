2a593ffdda88cc9df57303e9bccc010fb6affb4a
Mark Dickinson <monoposon@comcast.net>
2016-07-21 11:26:10 -0600

    finished

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,23 @@
 (function() {
   'use strict';
-    console.log('sanity check!');
+
+  var brush;
+  var pixel;
+
+  function newCanvas() {
+    var $canvas = $('#canvas');
+    for (var i = 0; i < 3402; i++) {
+      $canvas.append($('<div class="pixel"></div>'));
+    }
+  }
+  newCanvas();
+
+  $('.pixel').on('click', function(event) {
+      $(this).css('background-color', brush)
+  })
+
+  $('.paintColor').on('click', function() {
+    brush = $(this).css('background-color');
+    $('#currentColor').css({'background-color': brush})
+  });
 }());
